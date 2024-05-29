363bdb03350bd52fc7290fc6eed9f1daab602ade
Maria Clara Andrade <claramaria5751@gmailcom>
2024-05-29 16:05:52 -0300

    codigo pronto

--- jogo.py
+++ jogo.py
@@ -4,4 +4,25 @@ def jogo_adivinhacao():
     numero_secreto = random.randint(1, 100)
     tentativas = 0
 
+    print("Bem-Vindo ao jogo deadivinhação!")
+    print("Estou pensando em um numero de 1 a 100")
+
+    while  True: 
+        try:
+            palpite = int(input("Digite o seu palpite: "))
+            tentativas += 1
+
+            if palpite < numero_secreto:
+                print("Muito Baixo!")
+            elif palpite > numero_secreto:
+                print("Muito alto!")
+            else:
+                print(f"Parabéns! Você adivinhou o número em {tentativas} tentativas.")
+                break
+        except ValueError:
+            print("Por favor digite um número válido. ")
+
+if __name__ == "__main__":
+    jogo_adivinhacao()
+
     
